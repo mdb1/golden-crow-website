037599469b0adf8c5f485d01c2637a1d2110e49f
Federico Bustos Fierro <federico0812@gmail.com>
2026-08-14 17:32:34 -0300

    Italicize RareFriends brand references

--- backoffice/src/lib/app-version.ts
+++ backoffice/src/lib/app-version.ts
@@ -1,1 +1,1 @@
-export const BACKOFFICE_VERSION = "2.477";
+export const BACKOFFICE_VERSION = "2.478";
---
+++ pocket-genes/src/utils/brandFormatting.ts
@@ -0,0 +1,21 @@
+const RAREFRIENDS_PATTERN = /RareFriends(?:™)?/g;
+
+export function escapeHtml(value: string | number | null | undefined): string {
+  return String(value ?? '')
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
+}
+
+export function formatBrandHtml(value: string | number | null | undefined): string {
+  return String(value ?? '').replace(
+    RAREFRIENDS_PATTERN,
+    (match) => `<em class="pg-brand-name">${match}</em>`,
+  );
+}
+
+export function formatBrandText(value: string | number | null | undefined): string {
+  return formatBrandHtml(escapeHtml(value));
+}
